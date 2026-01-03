1754872f83d08414eebedac96b29c0e61e6d33a6
Akash Kumar Soni <123565762+akash-soni9@users.noreply.github.com>
2026-01-03 11:16:57 +0530

    Logistic Regression (validation to predict future)

---
+++ Logistic Regression.py
@@ -0,0 +1,131 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
+
+dataset = pd.read_csv(r'C:\Users\akash\OneDrive\Desktop\FSDS\ML\C\logit classification.csv')
+
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, -1].values
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=0)
+
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+
+
+classifier = LogisticRegression()
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+print(y_pred)
+
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+ac = accuracy_score(y_test, y_pred)
+print(ac)
+
+
+report = classification_report(y_test, y_pred)
+print(report)
+
+
+'''
+# Visualising the Train set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+'''
+
+dataset1 = pd.read_csv(r'C:\Users\akash\OneDrive\Desktop\FSDS\ML\C\2.LOGISTIC REGRESSION CODE\final1.csv')
+
+d2 = dataset1.copy()
+
+dataset1 = dataset1.iloc[:, 3:].values
+
+#scaling
+M = sc.fit_transform(dataset1)
+
+# creating empty dataframe
+y_pred1 = pd.DataFrame()
+
+
+d2['y_pred1'] = classifier.predict(M)
+
+d2.to_csv('final1.csv')
+
+import os
+os.getcwd()
+
+
+
+
+from sklearn.metrics import roc_auc_score, roc_curve
+y_pred_prob = classifier.predict_proba(X_test)[:, 1]
+
+auc_score = roc_auc_score(y_test, y_pred_prob)
+print(auc_score)
+
+fpr, tpr, threshold = roc_curve(y_test, y_pred_prob)
+
+plt.figure(figsize=(7,5))
+plt.plot(fpr, tpr, label = f'logistic regression (AUC = {auc_score:.2f})')
+plt.plot([0,1], [0, 1], 'k--')
+plt.xlabel("fpr")
+plt.ylabel("tpr")
+plt.title("ROC Curve")
+plt.legend(loc='lower right')
+plt.grid()
+plt.plot()
+
+
+
+
+
+
+
+
+
+
+
+
