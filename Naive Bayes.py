e0c278e3cd0becaee7911fdc397fb5102484cc5a
Akash Kumar Soni <123565762+akash-soni9@users.noreply.github.com>
2026-01-10 13:47:31 +0530

    Naive Bayes

---
+++ Naive Bayes.py
@@ -0,0 +1,119 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv(r"C:\Users\akash\OneDrive\Desktop\FSDS\ML\C\3rd - KNN\Social_Network_Ads.csv")
+
+X = dataset.iloc[:, [2,3]].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=0)
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+X_train = sc.fit_transform(X_train)
+X_test = sc.transform(X_test)
+
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+
+classifier.fit(X_train, y_train)
+
+y_pred = classifier.predict(X_test)
+
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+#confusion matrix
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+
+ac = accuracy_score(y_test, y_pred)
+print(ac)
+
+#bias
+bias = classifier.score(X_train, y_train)
+print('bias:',bias)
+#variance
+variance = classifier.score(X_test, y_test)
+print('variance:',variance)
+
+# ROC-AUC curve
+from sklearn.metrics import roc_auc_score, roc_curve
+
+# Predict probabilities
+y_pred_prob = classifier.predict_proba(X_test)[:, 1]
+
+# AUC score
+auc_score = roc_auc_score(y_test, y_pred_prob)
+
+# ROC curve
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
+
+#plt.figure(figsize=(8,6))
+plt.plot(fpr, tpr, color='blue', linewidth=2,
+         label=f'GaussianNB (AUC = {auc_score:.2f})')
+plt.plot([0, 1], [0, 1], 'k--', linewidth=1)
+
+plt.title('ROC Curve - Gaussian Naive Bayes')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.legend(loc='lower right')
+plt.grid(alpha=0.3)
+plt.show()
+
+
+from matplotlib.colors import ListedColormap
+
+X_set, y_set = X_train, y_train
+
+X1, X2 = np.meshgrid(
+    np.arange(X_set[:,0].min()-1, X_set[:,0].max()+1, step=0.01),
+    np.arange(X_set[:,1].min()-1, X_set[:,1].max()+1, step=0.01)
+)
+
+plt.contourf(
+    X1, X2,
+    classifier.predict(np.c_[X1.ravel(), X2.ravel()]).reshape(X1.shape),
+    alpha=0.75,
+    cmap=ListedColormap(('red', 'green'))
+)
+
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(
+        X_set[y_set == j, 0],
+        X_set[y_set == j, 1],
+        c=ListedColormap(('red', 'green'))(i),
+        label=j
+    )
+
+plt.title('Gaussian Naive Bayes (Training Set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary ')
+plt.legend()
+plt.show()
+
+
+
+# Visualising the Test set results
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Gausian Naive Bayes (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+
+
