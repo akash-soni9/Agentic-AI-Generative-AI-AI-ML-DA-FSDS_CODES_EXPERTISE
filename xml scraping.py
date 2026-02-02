bb96a38a89860dad4fed867169853d66f0602225
Akash Kumar Soni <123565762+akash-soni9@users.noreply.github.com>
2026-02-02 18:24:26 +0530

    Text extraction using xml scraping

---
+++ xml scraping.py
@@ -0,0 +1,34 @@
+import os
+os.chdir(r'C:\Users\akash\OneDrive\Desktop\FSDS\AI\NLP')
+
+import xml.etree.ElementTree as ET
+
+tree = ET.parse("769952.xml")
+root = tree.getroot()
+
+root = ET.tostring(root, encoding='utf8').decode('utf8')
+root
+
+import re, string, unicodedata
+import nltk 
+
+from bs4 import BeautifulSoup
+from nltk import word_tokenize, sent_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import LancasterStemmer, WordNetLemmatizer
+
+
+def strip_html(text):
+    soup = BeautifulSoup(text, "html.parser")
+    return soup.get_text()
+
+def remove_between_square_brackets(text):
+    return re.sub('\[[^]]*\]', "", text)
+
+def denoise_text(text):
+    text = strip_html(text)
+    text = remove_between_square_brackets(text)
+    rext = re.sub("  ", '', text)
+    return text
+
+sample = denoise_text(root)
